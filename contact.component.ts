820ca15e9cfca177516e85aa4668d1600b535a3c
NgoManhSon <110463164+NgoManhson@users.noreply.github.com>
2022-12-07 18:38:25 +0700

    Contact

---
+++ contact.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input,} from "@angular/core";
+
+@Component({
+  selector:'contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.css'],
+})
+export class ContactComponent{
+
+}
