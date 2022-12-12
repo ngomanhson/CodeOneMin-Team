f87870f815762ca2bd1a5804a22237fc82d6cc88
NgoManhSon <110463164+NgoManhson@users.noreply.github.com>
2022-12-12 23:24:42 +0700

    trang thanh toan

---
+++ pay.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input,} from "@angular/core";
+
+@Component({
+  selector:'thanhtoan',
+  templateUrl: './pay.component.html',
+  styleUrls: ['pay.css']
+})
+export class ThanhtoanComponent{
+
+}
