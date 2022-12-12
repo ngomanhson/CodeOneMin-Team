384c92debd7bb2d93968b042a63c66767b5f1250
NgoManhSon <110463164+NgoManhson@users.noreply.github.com>
2022-12-13 01:36:15 +0700

    dang nhap - dang ky

---
+++ dangky2.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input,} from "@angular/core";
+
+@Component({
+  selector:'dangky2',
+  templateUrl: './dangky2.component.html',
+  styleUrls: ['dangky2.css']
+})
+export class Dangky2Component {
+
+}
---
+++ dangnhap.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input,} from "@angular/core";
+
+@Component({
+  selector:'dangnhap',
+  templateUrl: './dangnhap.component.html',
+  styleUrls: ['dangnhap.css']
+})
+export class DangnhapComponent{
+
+}
